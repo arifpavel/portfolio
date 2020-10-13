41cfa1274f70813d05283885b06c2ad12fe1c9ce
Arif Pavel <pavelpatiya@gmail.com>
2020-10-13 13:55:17 +0600

    initial push with red color palate

---
+++ tailwind.config.js
@@ -0,0 +1,168 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.html',
+      './src/**/*.pug',
+      './src/**/*.md',
+      './docs/**/*.md',
+      './blog/**/*.md',
+    ],
+    options: {
+      whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+        'active',
+      ],
+    }
+  },
+  theme: {
+    extend: {
+      spacing: {
+        '80': '20rem',
+        '108': '27rem',
+      },
+      borderWidth: {
+        '14': '14px',
+      }
+    },
+    container: {
+      padding: '1rem'
+    },
+    colors: {
+      background: {
+        primary: 'var(--bg-background-primary)',
+        secondary: 'var(--bg-background-secondary)',
+        tertiary: 'var(--bg-background-tertiary)',
+
+        form: 'var(--bg-background-form)',
+      },
+
+      copy: {
+        primary: 'var(--text-copy-primary)',
+        secondary: 'var(--text-copy-hover)',
+      },
+
+      'border-color': {
+        primary: 'var(--border-border-color-primary)',
+      },
+
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+
+      green: {
+        100: '#f0fff4',
+        200: '#c6f6d5',
+        300: '#9ae6b4',
+        400: '#68d391',
+        500: '#48bb78',
+        600: '#38a169',
+        700: '#2f855a',
+        800: '#276749',
+        900: '#22543d',
+      },
+
+      red: {
+        100: '#B31332',
+        200: '#AB1333',
+        300: '#A31233',
+        400: '#9B1233',
+        500: '#931133',
+        600: '#8B1133',
+        700: '#831034',
+        800: '#7B1034',
+        900: '#741034',
+        1000: 'linear-gradient(to right, rgb(195, 20, 50), rgb(36, 11, 54));'
+      },
+
+      blue: {
+        100: '#ebf8ff',
+        200: '#bee3f8',
+        300: '#90cdf4',
+        400: '#63b3ed',
+        500: '#4299e1',
+        600: '#3182ce',
+        700: '#2b6cb0',
+        800: '#2c5282',
+        900: '#2a4365',
+      },
+
+      orange: {
+        100: '#fffaf0',
+        200: '#feebc8',
+        300: '#fbd38d',
+        400: '#f6ad55',
+        500: '#ed8936',
+        600: '#dd6b20',
+        700: '#c05621',
+        800: '#9c4221',
+        900: '#7b341e',
+      },
+
+      teal: {
+        100: '#e6fffa',
+        200: '#b2f5ea',
+        300: '#81e6d9',
+        400: '#4fd1c5',
+        500: '#38b2ac',
+        600: '#319795',
+        700: '#2c7a7b',
+        800: '#285e61',
+        900: '#234e52',
+      },
+
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c',
+      },
+    },
+    fontFamily: {
+      sans: [
+        'Nunito Sans',
+        'Roboto',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
+  },
+  variants: {
+    // Some useful comment
+  },
+  plugins: [
+    // Some useful comment
+  ]
+}
